dda06ac50ad27a880195240ce2a77680a962ad69
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-03-10 08:48:39 -0800

    change prettier to warn in eslint config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,15 +2,14 @@ module.exports = {
   env: {
     es6: true,
   },
-  extends: [ 
+  extends: [
     "eslint:recommended",
-  "plugin:import/errors",
-  "plugin:import/warnings",
-  "plugin:react/recommended",
-  "plugin:jsx-a11y/recommended",
-  "plugin:prettier/recommended"
-
-],
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+  ],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly",
@@ -28,8 +27,6 @@ module.exports = {
     "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
     "react/prop-types": "warn", // Checks if props have a defined prop type
     "no-unused-vars": "warn", // Checks if there are unused variables
-    "prettier/prettier": ["error", {}, {
-      "usePrettierrc": true
-    }]
+    "prettier/prettier": "warn",
   },
 };
