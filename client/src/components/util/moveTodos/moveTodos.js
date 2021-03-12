662cc245540062a956be0ea1d256b482311f57e3
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-03-12 10:53:06 -0800

    move todos in new structure

--- client/src/components/util/moveTodos/moveTodos.js
+++ client/src/components/util/moveTodos/moveTodos.js
@@ -18,101 +18,54 @@ import { getShiftedIds, findMatchingElementsInArrays } from "../util";
  * @param {string} draggableId Id of the todolist being dragged
  * @return {object} Asynchronously resolved Promise of Promise.all
  */
-export const moveTodos = (
-  jwt,
-  userId,
-  stateData,
-  source,
-  destination,
-  draggableId
-) => {
-  // Get todolist data being dragged
-  const todolist = stateData.todolists[source.droppableId];
-  // Copy the todo order array
-  const newTodoIds = Array.from(todolist.todoIds);
+export const moveTodos = (jwt, userId, stateData, source, destination, draggableId) => {
+  console.log(stateData, "stateData");
+  const { data } = stateData;
+  const { todolists } = data;
 
-  removeAndInsertArrayElement(
-    newTodoIds,
-    source.index,
-    destination.index,
-    draggableId
-  );
-
-  // Add the new todo order to the current todolist object
-  const newTodolist = {
-    ...todolist,
-    todoIds: newTodoIds,
-  };
-
-  // Add the new todolist with it's new todo order to a new state data object
-  const newStateData = {
-    ...stateData,
-    todolists: {
-      ...stateData.todolists,
-      [newTodolist.dndId]: newTodolist,
-    },
-  };
-
-  /*
-    get the original todoIds for order
-    the todoId order for the put request will be the same as the original order
-    Example is for moving todo-9 to end of the todos array
-    EX. todo-9, todo-8, todo-7, todo-6 ids => {id: 9},{id:  8}, {id: 7}, {id: 6}
-    The after the swap you want the new todoIds, but their data only.
-    EX. {...todo-8 data}, {...todo-7 data}, {...todo-6 data}, {...todo-9 data}
-    put request will be the ids of original array with new swapped data
-    EX.
-    {id: 9, ...todo-8 data}, {id:  8, ...todo-7 data},
-    { id: 7, ...todo - 6 data }, { id: 6, ...todo - 9 data }
-  */
-
-  // get the original todos order
-  const originalTodoIds = Array.from(todolist.todoIds);
-  // get the new todos order
-  const copyTodoIds = Array.from(newTodoIds);
-
-  // Get the todo ids that have shifted after moving the todo
-  // ShiftedTodoIds array to keep track of which todo ids have shifted
-  // OldTodoIds array to keep track of original order of todo ids
-  let [shiftedTodoIds, oldTodoIds] = getShiftedIds(
-    copyTodoIds,
-    originalTodoIds,
-    source.index,
-    destination.index
-  );
-
-  // Convert todo objects into an array
-  const todosAsArray = Object.entries(newStateData.todos);
-  const newTodos = findMatchingElementsInArrays(oldTodoIds, todosAsArray);
-
-  const copyNewTodos = Array.from(newTodos);
-
-  //  Get the matching todos with the shifted todos
-  shiftedTodoIds.forEach((todo, i) => {
-    todosAsArray.forEach((element, j) => {
-      if (todosAsArray[j][0].includes(todo)) {
-        copyNewTodos[i] = {
-          ...copyNewTodos[i],
-          content: todosAsArray[j][1].content,
-          createdAt: todosAsArray[j][1].createdAt,
-          firstName: todosAsArray[j][1].firstName,
-          lastName: todosAsArray[j][1].lastName,
-          todolistId: todosAsArray[j][1].todolistId,
-          todolistTitle: todosAsArray[j][1].todolistTitle,
-          updatedAt: todosAsArray[j][1].updatedAt,
-        };
-      }
-    });
+  // Get todolist data that the todo is in
+  const list = todolists.find((todolists, i) => {
+    if (todolists.todolist.dndId === source.droppableId) {
+      return todolists.todolist.dndId === source.droppableId;
+    }
   });
+
+  const { todolist, todos } = list;
+
+  console.log(todolist);
+
+  console.log(`todolist name: ${todolist.title}`);
+
+  const copyTodos = Array.from(todos);
+
+  console.log(`todos in todolist name ${todolist.title}`, copyTodos);
+
+  const todoMoved = todos.find((todos, i) => {
+    if (todos.dndId === draggableId) {
+      return todos.dndId === draggableId;
+    }
+  });
+
+  removeAndInsertArrayElement(copyTodos, source.index, destination.index, todoMoved);
+  // console.log(copyTodos);
+
+  const finalTodos = copyTodos.map((todo, i) => {
+    return {
+      ...todo,
+      id: todos[i].id,
+    };
+  });
+
+  console.log(`finalTodos that will sent as a post request`, finalTodos);
 
   const promises = [];
 
-  copyNewTodos.forEach((todo) => {
-    const { todolistId, id, content } = todo;
+  finalTodos.forEach((todo) => {
+    const { todolist_id, id, description } = todo;
     promises.push(
       axios.put(
-        SINGLE_TODO_URI(userId, todolistId, id),
-        { description: content },
+        SINGLE_TODO_URI(userId, todolist_id, id),
+        { description },
         {
           headers: {
             Authorization: jwt,
