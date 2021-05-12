e3a3de18569d999035bc0ea608e0583a2f0e30f2
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-05-11 17:10:09 -0700

    change back to heroku production uri

--- client/src/endpoints/endpoints.js
+++ client/src/endpoints/endpoints.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 // TODO Add later
-const productionUri = `https://localhost:53504`;
+const productionUri = `https://still-beyond-11172.herokuapp.com`;
 export const ROOT_URI = process.env.NODE_ENV === `production` ? productionUri : `http://localhost:5000`;
 
 // Login URI
