0143c9feb712fcd6e4761359662559c91cb392c5
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-05-12 22:12:11 -0700

    remove trailing forward slash from ROOT_URI in client endpoints

--- client/src/endpoints/endpoints.js
+++ client/src/endpoints/endpoints.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-undef */
 // TODO Add later
 const productionUri = process.env.REACT_APP_API_URL;
-export const ROOT_URI = process.env.NODE_ENV === `production` ? productionUri : `http://localhost:5000`;
+export const ROOT_URI = process.env.NODE_ENV === `production` ? productionUri : `http://localhost:5000/`;
 
 // Login URI
-export const LOGIN_URI = `${ROOT_URI}/api/login`;
+export const LOGIN_URI = `${ROOT_URI}api/login`;
 
 // all users
-export const ALL_USERS = `${ROOT_URI}/api/users`;
+export const ALL_USERS = `${ROOT_URI}api/users`;
 
 /**
  * Todolists URI
@@ -15,7 +15,7 @@ export const ALL_USERS = `${ROOT_URI}/api/users`;
  * @param {number} userId User's ID
  * @return {string} The URI/endpoint for a user's todolist(s).
  */
-export const TODOLISTS_URI = (userId) => `${ROOT_URI}/api/users/${userId}/todolists`;
+export const TODOLISTS_URI = (userId) => `${ROOT_URI}api/users/${userId}/todolists`;
 
 /**
  * Single todolist URI
@@ -24,7 +24,7 @@ export const TODOLISTS_URI = (userId) => `${ROOT_URI}/api/users/${userId}/todoli
  * @param {number} todolistId Todolist's ID
  * @return {string} The URI/endpoint for a user's specific todolist.
  */
-export const SINGLE_TODOLIST_URI = (userId, todolistId) => `${ROOT_URI}/api/users/${userId}/todolists/${todolistId}`;
+export const SINGLE_TODOLIST_URI = (userId, todolistId) => `${ROOT_URI}api/users/${userId}/todolists/${todolistId}`;
 
 /**
  * Todos URI
@@ -33,7 +33,7 @@ export const SINGLE_TODOLIST_URI = (userId, todolistId) => `${ROOT_URI}/api/user
  * @param {number} todolistId Todolist's ID
  * @return {string} The URI/endpoint for a user's todos in a specific todolist.
  */
-export const TODOS_URI = (userId, todolistId) => `${ROOT_URI}/api/users/${userId}/todolists/${todolistId}/todos`;
+export const TODOS_URI = (userId, todolistId) => `${ROOT_URI}api/users/${userId}/todolists/${todolistId}/todos`;
 
 /**
  * Single todo URI
@@ -44,7 +44,7 @@ export const TODOS_URI = (userId, todolistId) => `${ROOT_URI}/api/users/${userId
  * @return {string} The URI/endpoint for a user's specific todos in a specific todolist.
  */
 export const SINGLE_TODO_URI = (userId, todolistId, todoId) =>
-  `${ROOT_URI}/api/users/${userId}/todolists/${todolistId}/todos/${todoId}`;
+  `${ROOT_URI}api/users/${userId}/todolists/${todolistId}/todos/${todoId}`;
 
 /**
  *  Batch delete completed todos
@@ -55,7 +55,7 @@ export const SINGLE_TODO_URI = (userId, todolistId, todoId) =>
  * @return {string} The URI/endpoint for a user's completed todos in a specific todolist.
  */
 export const DELETE_COMPLETED_TODO_URI = (userId, todolistId) =>
-  `${ROOT_URI}/api/users/${userId}/todolists/${todolistId}/delete-completed-todos`;
+  `${ROOT_URI}api/users/${userId}/todolists/${todolistId}/delete-completed-todos`;
 
 /**
  *  Batch update todos to completed
@@ -66,4 +66,4 @@ export const DELETE_COMPLETED_TODO_URI = (userId, todolistId) =>
  * @return {string} The URI/endpoint for a user's completed todos in a specific todolist.
  */
 export const SET_TODOS_TO_COMPLETE = (userId, todolistId) =>
-  `${ROOT_URI}/api/users/${userId}/todolists/${todolistId}/batch-set-todos`;
+  `${ROOT_URI}api/users/${userId}/todolists/${todolistId}/batch-set-todos`;
