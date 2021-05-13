9465240fa6209efe34936e03a16cbf4ad87e6bf0
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-05-13 11:09:58 -0700

    remove serving static files from server for now

--- server/index.js
+++ server/index.js
@@ -20,14 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static(path.resolve(__dirname, "/client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static(path.resolve(__dirname, "/client/build")));
 
-  app.get("*", (_req, res) => {
-    res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
-  });
-}
+//   app.get("*", (_req, res) => {
+//     res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
+//   });
+// }
 
 const jsonErrorHandler = async (err, req, res, next) => {
   res.status(500).send({ error: err });
